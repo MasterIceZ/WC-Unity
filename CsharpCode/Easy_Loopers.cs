fc2cd9c52cc1b6df95be0d0d33ca6344ac4fd0c3
AiceAeng <64594596+MasterIceZ@users.noreply.github.com>
2020-06-21 15:43:10 +0700

    Loopers

---
+++ CsharpCode/Easy_Loopers.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+  For loop like C/C++
+  Syntax :
+    for(int i=0;i<n;i++)
+   //Ez
+*/
+public class filename : MonoBehaviour
+{
+  void Start()
+  {
+    for(int i=0;i<n;i++){
+      /*
+        works
+      */
+    }
+    
+    
+    
+  }
+  
+  void Update()
+  {
+  
+  }
+}
