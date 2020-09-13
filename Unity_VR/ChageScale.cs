4eabb3978ff1f15ee0abd93aefbc428024a8d3fd
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-09-13 15:47:43 +0700

    Create ChageScale.cs

---
+++ Unity_VR/ChageScale.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChageScale : MonoBehaviour
+{
+    public float Scaler;
+    void Start()
+    {
+        Scaler =  Random.Range(0,1);
+    }
+
+    void Update()
+    {
+        transform.localScale += new Vector3(Scaler,Scaler,Scaler);
+    }
+}
