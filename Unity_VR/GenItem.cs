d04c0f851402ea770c07a7d891087d39811841ae
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-09-13 16:58:52 +0700

    Create GenItem.cs

---
+++ Unity_VR/GenItem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenItem : MonoBehaviour
+{
+    public List <GameObject> Breakable;
+    public bool on;
+    private int idx;
+    void Update()
+    {
+        if(on)
+        {
+            StartCoroutine(Waiting());
+        }
+    }
+    IEnumerator Waiting()
+    {
+        idx = SelectingItem();
+        Instantiate(Breakable[idx],transform.position,transform.rotation);
+        on = false;
+        yield return new WaitForSeconds(2);
+        on = true;
+    }
+    public int SelectingItem()
+    {
+        int Select = Random.Range(0,2);
+        if(Select == 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
