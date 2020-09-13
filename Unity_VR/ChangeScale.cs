d6e1da24a500915653ba2832babfb9b03f00e594
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-09-13 15:55:06 +0700

    Update and rename ChageScale.cs to ChangeScale.cs

--- Unity_VR/ChangeScale.cs
+++ Unity_VR/ChangeScale.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChageScale : MonoBehaviour
+public class ChangeScale : MonoBehaviour
 {
     public float Scaler;
     void Start()
